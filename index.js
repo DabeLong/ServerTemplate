76c83eaa8cb34c3567f81f694fd679178514033e
dmlong <davidmingtianlong@gmail.com>
2016-10-12 16:55:01 -0500

    python

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var stormpath = require('express-stormpath')
-var app = express()
+var express = require('express');
+var stormpath = require('express-stormpath');
+var app = express();
  
 app.use(stormpath.init(app, {
   application:process.env.STORMPATH_URL,
@@ -11,36 +11,53 @@ app.use(stormpath.init(app, {
   // web: {
   //   produces: ['application/json']
   // }
-}))
+}));
 
 // GET 
 app.get('/', function(req, res) {
   res.send("Hey there!")
-})
+});
 
+
+app.get('/calculate', function(req, res){
+	var spawn = require('child_process').spawn,
+    py    = spawn('python', ['compute_input.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
+
+	py.stdout.on('data', function(data){
+	  dataString += data.toString();
+	});
+	py.stdout.on('end', function(){
+	  console.log('Sum of numbers=',dataString);
+	  res.send(dataString);
+	});
+	py.stdin.write(JSON.stringify(data));
+	py.stdin.end();
+});
 
 
 // // GET note API call
 // app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
 //   res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!"})
-// })
+// });
  
 
 // // POST note API call
-// var bodyParser = require('body-parser')
+// var bodyParser = require('body-parser');
  
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
  
 // app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
 //   if(!req.body.notes || typeof req.body.notes != "string") {
-//     res.status(400).send("400 Bad Request")
+//     res.status(400).send("400 Bad Request");
 //   }
  
-//   req.user.customData.notes = req.body.notes
-//   req.user.customData.save()
-//   res.status(200).end()
-// }) 
+//   req.user.customData.notes = req.body.notes;
+//   req.user.customData.save();
+//   res.status(200).end();
+// }) ;
 
 
-app.listen(process.env.PORT || 3000) // port number
+app.listen(process.env.PORT || 3000); // port number
