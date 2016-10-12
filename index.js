b91096b7b6b63e67a68a9cffb112eb9fe1d8e05c
dmlong <davidmingtianlong@gmail.com>
2016-10-11 20:09:47 -0500

    index

--- index.js
+++ index.js
@@ -14,33 +14,33 @@ app.use(stormpath.init(app, {
 }))
 
 // GET 
-app.get('/', stormpath.apiAuthenticationRequired, function(req, res) {
+app.get('/', function(req, res) {
   res.send("Hey there!")
 })
 
 
 
-// GET note API call
-app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
-  res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!"})
-})
+// // GET note API call
+// app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+//   res.json({notes: req.user.customData.notes || "This is your notebook. Edit this to start saving your notes!"})
+// })
  
 
-// POST note API call
-var bodyParser = require('body-parser')
+// // POST note API call
+// var bodyParser = require('body-parser')
  
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: false}))
  
-app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
-  if(!req.body.notes || typeof req.body.notes != "string") {
-    res.status(400).send("400 Bad Request")
-  }
+// app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+//   if(!req.body.notes || typeof req.body.notes != "string") {
+//     res.status(400).send("400 Bad Request")
+//   }
  
-  req.user.customData.notes = req.body.notes
-  req.user.customData.save()
-  res.status(200).end()
-}) 
+//   req.user.customData.notes = req.body.notes
+//   req.user.customData.save()
+//   res.status(200).end()
+// }) 
 
 
 app.listen(process.env.PORT || 3000) // port number
